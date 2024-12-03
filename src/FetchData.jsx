9ad7e0735f8f153af1267c8753b0e9842c54a2d3
AmitKumarPrajapati <amitprajapati1993.ap@gmail.com>
2024-12-03 17:38:54 +0530

    Data fetch component

--- src/FetchData.jsx
+++ src/FetchData.jsx
@@ -2,6 +2,18 @@ import React, { useState, useEffect } from 'react';
 
 const FetchData = () => {
   const [posts, setPosts] = useState([]);
+  // const [currentItems, setCurrentItems]= useState([])
+  const [currentPage, setCurrentPage] = useState(1);
+  const [ pageLimit ] = useState(10)
+
+  const handleClick = (currentPage) => {
+    setCurrentPage(currentPage)
+  }
+
+  const indexOfLastItem =  currentPage * pageLimit;
+  const indexOfFirstItem = indexOfLastItem - pageLimit;
+  const currentItems = posts.slice(indexOfFirstItem, indexOfLastItem);
+  const totalPages = Math.ceil(posts.length / pageLimit)
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
@@ -19,15 +31,25 @@ const FetchData = () => {
           <th>Title</th>
           <th>Body</th>
         </tr>
-        {posts?.map((post) => (
+        {currentItems?.map((post) => (
           <tr>
-            <td>{post.id}</td>
             <td>{post.userId}</td>
+            <td>{post.id}</td>
             <td>{post.title}</td>
             <td>{post.body}</td>
           </tr>
         ))}
       </table>
+      <div style={{display: 'flex', marginTop: '20px'}}>
+        {
+          Array.from({length: totalPages}, (_,i) => i + 1).map((pageNum)=> (
+            <button 
+            disabled={currentPage === pageNum}
+            
+            onClick={()=> handleClick(pageNum)}>{pageNum}</button>
+          ))
+        }
+      </div>
     </div>
   );
 };
